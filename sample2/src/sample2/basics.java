d4f4ac2291d0bf8002dcd1ef2de43de2311c1040
HP <HP@LAPTOP-1SJ6Q760>
2023-07-27 16:05:29 +0530

    Raja

---
+++ sample2/src/sample2/basics.java
@@ -0,0 +1,16 @@
+package sample2;
+
+import java.util.Scanner;
+
+public class basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter name:");
+		Scanner sc =new Scanner(System.in);
+		String s=sc.nextLine();
+		System.out.println("good morning "+s);
+
+	}
+
+}
