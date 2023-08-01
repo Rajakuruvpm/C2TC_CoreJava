45b1954d5d05e20bf8d85682a271000bdc5df5c5
HP <HP@LAPTOP-1SJ6Q760>
2023-08-01 10:25:59 +0530

    raja

--- Core_java/src/com/cg/daytwo/operator.java
+++ Core_java/src/com/cg/daytwo/operator.java
@@ -1,9 +1,22 @@
 package com.cg.daytwo;
 
+import java.util.Scanner;
+
 public class operator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("enter first no:");
+		Scanner sc =new Scanner(System.in);
+		int a=sc.nextInt();
+		System.out.println("enter second no:");
+		int b=sc.nextInt();
+		
+		System.out.println("addition:"+(a+b));
+		System.out.println("subtraction:"+(a-b));
+		System.out.println("multiplication:"+(a*b));
+		System.out.println("division:"+(a/b));
+
 
 	}
 
